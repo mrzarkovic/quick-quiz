3bc176011ca9487597961679f760e6eb02b9bd34
Milos Zarkovic <milos.zarkovic@kupujemprodajem.com>
2018-03-27 16:51:48 +0200

    components and questions list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,97 @@
 import React, {Component} from 'react';
 
+import QuestionList from './quiz/QuestionList.jsx'
+
 class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            questions: [
+                {
+                    id: 1,
+                    text: 'What is your name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Michael'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Milos'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Brad'
+                        }
+                    ],
+                    correct: 'b'
+                },
+                {
+                    id: 2,
+                    text: 'What is your last name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Zarkovic'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Zek'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Monte'
+                        }
+                    ],
+                    correct: 'a'
+                },
+                {
+                    id: 3,
+                    text: 'What is your mother\'s name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Lucy'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Donna'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Mirjana'
+                        }
+                    ],
+                    correct: 'c'
+                },
+                {
+                    id: 4,
+                    text: 'What is your brothers name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Bojan'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Zoran'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Hose'
+                        }
+                    ],
+                    correct: 'a'
+                }
+            ],
+            score: 0,
+            current: 1
+        }
+    }
     render () {
         return (
             <div className="container">
-                App
+                <QuestionList {...this.state} />
             </div>
         );
     }
